7e612302be5d98595858d7abb46e36b4342b1d7f
Siddharth Ajay <sajay@uci.edu>
2024-10-26 20:31:39 -0700

    technically works ?

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,8 +16,12 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    print("E")
-    return list()
+    links = []
+    if resp.status == 200:
+        soup = BeautifulSoup(resp.raw_response.content,'html.parser')
+        links = [link['href'] for link in soup.find_all('a',href=True)]
+        #print(links)
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
