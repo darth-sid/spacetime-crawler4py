f46aa3b9dd42f8c88dfc3d824956779224e48466
Siddharth Ajay <sid.ajay05@gmail.com>
2024-10-31 00:07:04 -0700

    restrictions

--- scraper.py
+++ scraper.py
@@ -1,19 +1,38 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 from bs4 import BeautifulSoup
 import shelve
-import os
 
-SAVE_PATH = "stats"
+save = shelve.open("unique_links")
 
-if not os.path.exists(SAVE_PATH+".db"):
-    save = shelve.open(SAVE_PATH)
-    save['links'] = set()
-else:
-    save = shelve.open(SAVE_PATH)
+def normalized_hash(parsed_url):
+    # queries to ignore
+    ignore = {'utm_source','utm_medium','utm_campaign','utm_term','utm_content',
+              'sessionid','sessid','sid','phpsessid','aspsessionid','jsessionid',
+              'date','month', 'year', 'time','calendar','schedule',
+              'ref','referrer','src','sort','order','orderby','direction','view','display',
+              'clid','click_id','aff_id','aid','affilliate_id','aff_sub', 'banner_id', 'campaign_id',
+              }
+    netloc = parsed_url.netloc
+    if netloc.startswith("www."):
+        netloc = netloc[4:]
+
+    path = parsed_url.path
+    if path.endswith("index.html"):
+        path = path[:-10]
+    elif path.endswith("index.htm"):
+        path = path[:-9]
+
+    query = parse_qs(parsed_url.query)
+    for param in query:
+        if param in ignore:
+            query.pop(param, None)
+    query = urlencode(query, doseq=True)
+
+    url = urlunparse(('',netloc,path,'',query,''))
+    return str(hash(url))
 
 def scraper(url, resp):
-    print(save['links'])
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
@@ -37,6 +56,10 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    
+    #ignore calendars, login pages, 
+    banned_paths = ['login','calendar',] # TODO
+
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
@@ -50,7 +73,11 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|ttf|otf|woff|woff2|eot|fon"
+            + r"|img|pkg|exe|msi|sql|db|mdb|log|sqlite"
+            + r"|cpp|py|pix|bib|war|ini|o|a|lib|obj|ini|config"
+            + r"|raw|ods|key|odp|ods|numbers|bat|sh|bak|swp"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|z|xz|lz|tgz|tbz|apk|ipa)$", parsed.path.lower())
         if not valid:
             return False
 
@@ -63,12 +90,24 @@ def is_valid(url):
         if not valid_domain:
             return False
 
-        link = (parsed.scheme, parsed.netloc, parsed.path, parsed.params, parsed.query)
-        if link in save['links']:
+        for path in banned_paths:
+            if re.search(path, parsed.path, re.IGNORECASE) is not None:
+                return False
+
+        # check for xxxx-xx-xx in queries (sign of a calendar)
+        if re.search(r"\b\d{4}-\d{2}-\d{2}\b", parsed.query) is not None:
             return False
-        save['links'].add(link)
 
+        #TODO: check for repeated directories and filter useless pages and low info pages
+        
+        # cache unique urls visited
+        url = f"{parsed.netloc}.{parsed.path}.{parsed.params}.{parsed.query}"
+        urlhash = normalized_hash(parsed)
+        if urlhash in save:
+            return False
+        save[urlhash] = url
 
+        
         return True
 
     except TypeError:
